ebc1374f0a277de72ed8eb5c4e1f529da8957cfa
Gullipalli Vijaya Aditya <vijayaaditya1996@gmail.com>
2018-05-21 17:07:47 +0530

    changes done

--- app/src/main/java/com/example/adity/welcomescreen/MainActivity.java
+++ app/src/main/java/com/example/adity/welcomescreen/MainActivity.java
@@ -4,18 +4,21 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+
 import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     LinearLayout l1,l2;
-    Button btnsub;
+    TextView textsub;
     Animation uptodown,downtoup;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        btnsub = (Button)findViewById(R.id.buttonsub);
+        textsub = (TextView)findViewById(R.id.textsub);
         l1 = (LinearLayout) findViewById(R.id.l1);
         l2 = (LinearLayout) findViewById(R.id.l2);
         uptodown = AnimationUtils.loadAnimation(this,R.anim.uptodown);
